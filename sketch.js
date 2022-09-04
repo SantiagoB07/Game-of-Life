3824e8f4fd3ab31963768e5cd1652cd814a28e34
SantiagoB07 <75955930+SantiagoB07@users.noreply.github.com>
2022-09-03 21:56:51 -0500

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,101 @@
+function setup() {
+  createCanvas(600, 400);
+  columnas = width / dimension
+  filas = height / dimension;
+  
+  arreglo = crearArreglo2D(columnas, filas);
+  nuevo_arreglo = crearArreglo2D(columnas, filas);
+  
+  
+  for (let i = 0; i < columnas; i++)
+  {
+    for (let j = 0; j < filas; j++)
+    {
+      arreglo[i][j] = floor(random(2));
+    }
+  }
+  
+  
+  
+}
+
+function draw() {
+  background(0);
+  
+  NuevaGeneración();
+  for (let i = 1; i < columnas - 1; i++)
+    {
+    for (let j = 1; j < filas - 1; j++)
+      {
+      let x = i * dimension;
+      let y = j * dimension;
+
+      if (arreglo[i][j] == 1)
+          {
+          fill(255, 0, 0);
+          rect(x, y, dimension, dimension);
+          }
+      }
+    }
+  }
+
+
+//Definir variables
+let dimension = 20;
+let columnas;
+let filas;
+let arreglo;
+let nuevo_arreglo;
+
+//Crear el arreglo 2D
+function crearArreglo2D(columnas, filas)
+{
+  arreglo = new Array(columnas);
+  for (let i = 0; i < arreglo.length; i++)
+  {
+    arreglo[i] = new Array(filas);
+  } 
+  return arreglo;
+}
+
+//Crear nueva generación
+function NuevaGeneración(){
+  //Contar vecinos
+  for (let x = 1; x < columnas -1; x++)
+    {
+      for (let y = 1; x < filas -1; y++)
+        {
+          let vecinos = 0;
+          for (let i = -1; i <= 1; i++)
+            {
+              for (let j = -1; j <= 1; j++)
+                {
+                  vecinos += arreglo[x+i][y+j]
+                }
+            }
+          vecinos -= arreglo[x][y];
+          
+          //Reglas del juego de la vida
+          if (arreglo[x][y] == 0 && vecinos == 3)
+            {
+              nuevo_arreglo[x][y] = 1;
+            }
+          else if (arreglo[x][y] == 1 && (vecinos < 2 || vecinos > 3))
+            {
+              nuevo_arreglo[x][y] = 0;
+            }
+          else
+            {
+              nuevo_arreglo[x][y] = arreglo[x][y];
+            }
+          }
+      }
+  
+  //Cambiar arreglos
+  let arreglo_temporal = arreglo;
+  arreglo = nuevo_arreglo;
+  nuevo_arreglo = arreglo_temporal;
+    
+  }       
+              
+            
